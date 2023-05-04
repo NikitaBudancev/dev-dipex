4c9210f522f6574ac6947798c677b417d28480e2
Nikita <nikita@Nikitas-MacBook-Air.local>
2023-05-04 10:49:11 +0300

    commit

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -12,7 +12,7 @@ export default defineNuxtConfig({
       ],
     },
   },
-  css: ["~/assets/css/main.css", "vue-final-modal/style.css"],
+  css: ["~/assets/css/main.css"],
   postcss: {
     plugins: {
       tailwindcss: {},
